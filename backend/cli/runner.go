6622ffd8509ef9a0303f504e1ed4de63c17a8bdd
wolewd <williams.shc@gmail.com>
2026-03-05 16:43:20 +0700

    modify gitignore and cli runner

--- backend/cli/runner.go
+++ backend/cli/runner.go
@@ -3,10 +3,16 @@ package cli
 import (
 	"context"
 	"os/exec"
+	"syscall"
 )
 
 func RunPythonCLI(ctx context.Context, pythonPath string, scriptPath string, args ...string) ([]byte, error) {
 	cmdArgs := append([]string{scriptPath}, args...)
 	cmd := exec.CommandContext(ctx, pythonPath, cmdArgs...)
+
+	cmd.Cancel = func() error {
+		return cmd.Process.Signal(syscall.SIGKILL)
+	}
+
 	return cmd.CombinedOutput()
 }
